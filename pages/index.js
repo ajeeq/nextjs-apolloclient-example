b325aae5918b533b532364be843aec3808963372
heiz99 <haziqazfar041@gmail.com>
2020-09-16 18:31:25 +0800

    added error handling (try..catch block)

--- pages/index.js
+++ pages/index.js
@@ -19,34 +19,40 @@ const QUERY = gql`
 const Index = () => {
   const { loading, data } = useQuery(QUERY);
 
-  if (loading || !data) {
-    return <h1>loading...</h1>;
+  try {
+    if (loading || !data) {
+      return <h1>loading...</h1>;
+    }
+    else {
+      return (
+        <div>
+          <p>Country name: {data.country.name}</p>
+          <p>Native: {data.country.native}</p>
+          <p>Capital: {data.country.capital}</p>
+          <p>Emoji: {data.country.emoji}</p>
+          <p>Currency: {data.country.currency}</p>
+          <p>
+            {
+              data.country.languages.map((obj, i) => {
+                return "Language code: " + obj.code;
+              })
+            }
+          </p>
+          <p>
+            {
+              data.country.languages.map((obj, i) => {
+                return "Language name: " + obj.name;
+              })
+            }
+          </p>
+        </div>
+        
+      );
+    };
   }
-
-  return (
-    <div>
-      <p>Country name: {data.country.name}</p>
-      <p>Native: {data.country.native}</p>
-      <p>Capital: {data.country.capital}</p>
-      <p>Emoji: {data.country.emoji}</p>
-      <p>Currency: {data.country.currency}</p>
-      <p>
-        {
-          data.country.languages.map((obj, i) => {
-            return "Language code: " + obj.code;
-          })
-        }
-      </p>
-      <p>
-        {
-          data.country.languages.map((obj, i) => {
-            return "Language name: " + obj.name;
-          })
-        }
-      </p>
-    </div>
-    
-  );
-};
+  catch(err) {
+    return <h1>Data not found1, please check the query!</h1>;
+    }
+  }
 
 export default Index;
